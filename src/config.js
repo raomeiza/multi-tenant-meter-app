54b53cc17a93ef555e1f17bb05da39e76c8c7585
Raheem A Omeiza <blesseth.omeiza@gmail.com>
2024-10-03 10:44:39 +0100

    added online api server

--- src/config.js
+++ src/config.js
@@ -7,5 +7,5 @@ const config = {
   borderRadius: 12
 };
 
-export const API_BASE_URL = 'http://localhost:5000'
+export const API_BASE_URL = 'https://mutli-tenant-meter-api.onrender.com'
 export default config;
